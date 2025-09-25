fcecd781dbcb84762aab309168de118886361a33
Ayoola <ayoolagabriel2712@gmail.com>
2025-09-25 19:11:33 +0100

    Resolve Device Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { DeviceProvider } from './DeviceContext.jsx'
+import { DeviceProvider } from './components/DeviceContext.jsx'
 import { registerSW } from 'virtual:pwa-register'
 
 registerSW({ immediate:true })
